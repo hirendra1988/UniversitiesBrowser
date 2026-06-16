a307c1b38b19e565d9311bff7c4ddee30d6750cb
Hirendra Sharma <hks.it11@gmail.com>
2026-06-16 16:54:00 +0530

    Initial Commit

---
+++ UniversitiesBrowserTests/UniversitiesBrowserTests.swift
@@ -0,0 +1,19 @@
+//
+//  UniversitiesBrowserTests.swift
+//  UniversitiesBrowserTests
+//
+//  Created by Hirendra Sharma on 16/06/26.
+//
+
+import Testing
+@testable import UniversitiesBrowser
+
+struct UniversitiesBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
